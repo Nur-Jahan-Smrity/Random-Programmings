1d9636b522f4fca08096a125911901adb953b8a2
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-10-30 10:55:17 +0600

    Create efficient_Prime_factor.cpp

---
+++ efficient_Prime_factor.cpp
@@ -0,0 +1,32 @@
+//efficient_Prime_factor.cpp
+#include <bits/stdc++.h>
+using namespace std;
+void primeFactors(int n)
+{
+	while (n % 2 == 0)
+	{
+		cout << 2 << " ";
+		n = n/2;
+	}
+	for (int i = 3; i <= sqrt(n); i = i + 2)
+	{
+		while (n % i == 0)
+		{
+			cout << i << " ";
+			n = n/i;
+		}
+	}
+	if (n > 2)
+		cout << n << " ";
+}
+int main()
+{
+	int n;
+	cin>>n;
+	primeFactors(n);
+	return 0;
+}
+/*
+12
+2 2 3
+*/
