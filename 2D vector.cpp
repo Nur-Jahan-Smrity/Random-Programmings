087f097363f07ea323162e1cb00e8340a6759c8a
Nur-Jahan-Smrity <67903050+Nur-Jahan-Smrity@users.noreply.github.com>
2023-11-19 15:48:22 +0600

    Create 2D vector.cpp

---
+++ 2D vector.cpp
@@ -0,0 +1,23 @@
+/*
+2D vector.cpp
+*/
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+	vector<vector<int>> vect
+	{
+		{1, 2},
+		{4, 5, 6},
+		{7, 8, 9, 10}
+	};
+	for (int i = 0; i < vect.size(); i++)
+	{
+		for (int j = 0; j < vect[i].size(); j++)
+		{
+			cout << vect[i][j] << " ";
+		}
+		cout << endl;
+	}
+	return 0;
+}
